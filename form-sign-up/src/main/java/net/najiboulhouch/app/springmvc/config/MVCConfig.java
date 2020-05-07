ad3e21f946785f6d8c3dad37bdbc8cadc04f3793
developpersgroups <developpersgroups@gmail.com>
2020-05-07 18:49:30 +0000

    Organise imports packages

--- form-sign-up/src/main/java/net/najiboulhouch/app/springmvc/config/MVCConfig.java
+++ form-sign-up/src/main/java/net/najiboulhouch/app/springmvc/config/MVCConfig.java
@@ -3,7 +3,6 @@
  */
 package net.najiboulhouch.app.springmvc.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
